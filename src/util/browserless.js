401100a6d21fbfc014ce1f553bc28c8e1a5993f2
lawrence.li <hi@lawrenceli.me>
2024-05-14 00:24:15 +0800

    feat: chromium args

--- src/util/browserless.js
+++ src/util/browserless.js
@@ -1,26 +1,25 @@
 'use strict'
 
-// const createBrowser = require("browserless");
+const createBrowser = require('browserless')
 const chromium = require('@sparticuz/chromium')
 const puppeteer = require('puppeteer')
-// const path = require("path");
-// const { TMP_FOLDER } = require("../constant");
+const path = require('path')
+const { TMP_FOLDER } = require('../constant')
 
-// const getArgs = () => {
-//   const PUPPETEER_DIR = path.join(TMP_FOLDER, `puppeteer-${Date.now()}`);
-//   const DATA_DIR = path.join(PUPPETEER_DIR, "profile");
-//   const CACHE_DIR = path.join(PUPPETEER_DIR, "cache");
+const getArgs = () => {
+  const PUPPETEER_DIR = path.join(TMP_FOLDER, `puppeteer-${Date.now()}`)
+  const DATA_DIR = path.join(PUPPETEER_DIR, 'profile')
+  const CACHE_DIR = path.join(PUPPETEER_DIR, 'cache')
 
-//   const args = createBrowser.driver.defaultArgs.concat([
-//     "--allow-running-insecure-content", // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
-//     "--disk-cache-size=33554432", // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
-//     "--enable-features=SharedArrayBuffer", // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
-//     `--disk-cache-dir=${CACHE_DIR}`,
-//     `--user-data-dir=${DATA_DIR}`,
-//   ]);
-
-//   return { PUPPETEER_DIR, DATA_DIR, CACHE_DIR, args };
-// };
+  const args = createBrowser.driver.defaultArgs.concat([
+    '--allow-running-insecure-content', // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
+    '--disk-cache-size=33554432', // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
+    '--enable-features=SharedArrayBuffer', // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+    `--disk-cache-dir=${CACHE_DIR}`,
+    `--user-data-dir=${DATA_DIR}`
+  ])
+  return { PUPPETEER_DIR, DATA_DIR, CACHE_DIR, args }
+}
 
 // const browser = createBrowser({
 //   args: getArgs().args,
@@ -31,7 +30,7 @@ chromium.setGraphicsMode = false
 
 chromium.executablePath().then(e => {
   const browser = puppeteer.launch({
-    args: chromium.args,
+    args: getArgs().args,
     defaultViewport: chromium.defaultViewport,
     executablePath: e,
     headless: chromium.headless
